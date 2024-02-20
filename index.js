dd59bbe820c013f08e50a5ce3b471f6369eb978f
Minh Nguyen <minhnguyen93@outlook.com>
2024-02-20 13:56:26 +0200

    Make the route /api/authors and returns the number of blogs for each author and the total number of likes in descending order (exercise 13.16)

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const { errorHandler } = require('./util/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorsRouter = require('./controllers/authors')
 
 app.use(express.json())
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/authors', authorsRouter)
 app.use(errorHandler)
 
 const start = async () => {
